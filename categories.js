58fd36d97dc3362e6c84e5781436788288efb847
Christopher Longo <christopheralongo@gmail.com>
2025-08-14 11:19:59 -0400

    Second Commit

--- categories.js
+++ categories.js
@@ -1,8 +1,26 @@
 const CATEGORIES = [
-    { id: "food", label: "Food", isIncome: false },
-    { id: "salary", label: "Salary", isIncome: true },
-    { id: "clothing",label: "Clothing", isIncome: false },
-    { id: "games", label: "Games", isIncome: false }
+{ id: "Car Repair", label: "Car Repair", isIncome: false },
+{ id: "Clothing", label: "Clothing", isIncome: false },
+{ id: "Credit Card", label: "Credit Card", isIncome: false },
+{ id: "Dining", label: "Dining", isIncome: false },
+{ id: "Groceries", label: "Groceries", isIncome: false },
+{ id: "Gym", label: "Gym", isIncome: false },
+{ id: "House Repair", label: "House Repair", isIncome: false },
+{ id: "Leisure", label: "Leisure", isIncome: false },
+{ id: "Medical", label: "Medical", isIncome: false },
+{ id: "Pets", label: "Pets", isIncome: false },
+{ id: "Personal Expenses", label: "Personal Expenses", isIncome: false },
+{ id: "Rent/Mortgage", label: "Rent/Mortgage", isIncome: false },
+{ id: "Sub", label: "Sub", isIncome: false },
+{ id: "Transport", label: "Transport", isIncome: false },
+{ id: "Tuition", label: "Tuition", isIncome: false },
+{ id: "Utilities", label: "Utilities", isIncome: false },
+
+{ id: "Freelance", label: "Freelance", isIncome: true },
+{ id: "Lottery", label: "Lottery", isIncome: true },
+{ id: "Other Income", label: "Other Income", isIncome: true },
+{ id: "Salary", label: "Salary", isIncome: true },
+{ id: "Savings", label: "Savings", isIncome: true }
 ];
 
 function renderCategorySelect(selectId, categories) {
